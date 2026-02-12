05a72ead6f329bcbbf20860966381bc8fb40b332
tanishkak67 <tanishkakumari393@gmail.com>
2026-02-12 10:19:55 +0530

    Add files via upload
    checking if no equal

---
+++ checking if no equal.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int x,y;
+    printf(" enter first number \n");
+    scanf("%d",&x);
+    printf("enter second number\n");
+    scanf("%d",&y);
+    if (x==y)
+   { 
+     printf("numbers are equal");
+   }
+    else
+   {
+     printf("not equal");
+   }
+    return 0 ;   
+}
