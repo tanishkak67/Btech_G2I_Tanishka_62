61b3d3ba117615d38441dd95328dbc65255119ed
tanishkak67 <tanishkakumari393@gmail.com>
2026-02-12 09:34:05 +0530

    Add files via upload
    1ST

---
+++ 1ST.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main() 
+{
+    int a,b,c,d,e;
+    float sum, per;
+    printf("enter the subject marks:");
+    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+    sum= a+b+c+d+e
+    per= (sum/500)*100
+    if(per>=90&& per<=100)}
+    {
+        printf("GRADE A");
+    }
+    else if(per>=80&& per<90)
+    {
+        printf("GRADE B");
+    }
+}
